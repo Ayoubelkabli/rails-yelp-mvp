e5abfc101bdf5220895279eb11c19cd404ff1e00
ayoub elkabli <125612305+Ayoubelkabli@users.noreply.github.com>
2024-01-17 16:46:47 +0100

    second challenge

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gem "nokogiri"
+
 ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
@@ -25,6 +27,7 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
+gem "simple_form"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+# Create 5 restaurants
+restaurants = [
+  { name: 'LittleMamma', address: '123 hello st', category: 'italian', phone_number: '19793002' },
+  { name: 'BigMamma', address: '45 hay hassani', category: 'chinese', phone_number: '0973209720' },
+  { name: 'MediumMama', address: '78 sidi maarof', category: 'japanese', phone_number: '0830031973' },
+  { name: 'TinyMamma', address: '12 bd cali', category: 'french', phone_number: "8297310813" },
+  { name: 'HugeMamma', address: '20 bd sale', category: 'belgian', phone_number: '831083108' }
+]
+
+restaurants.each do |restaurant_data|
+  restaurant = Restaurant.create!(restaurant_data)
+
+  Review.create!(content: "Great food at #{restaurant.name}", rating: rand(1..5), restaurant: restaurant)
+end
